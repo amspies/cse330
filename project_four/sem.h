9d1e56d80b17b07b5d6d3e17ecdc54471e36a0e7
amspies <ams11195@gmail.com>
2016-11-14 18:11:41 -0700

    Create sem.h

---
+++ project_four/sem.h
@@ -0,0 +1,38 @@
+#ifndef SEM_H
+#define SEM_H
+
+#include "threads.h"
+
+struct semaphore {
+	int value;
+	struct TCB_t *queue;
+};
+
+void newSemaphore(struct semaphore *sem, int value)
+{
+	sem->value = value;
+}
+
+void P(struct semaphore *sem)
+{
+	sem->value--;
+	if (sem->value < 0) {
+		rotateQueue(&runQ);
+		struct TCB_t *currThread = delQueue(&runQ);
+		addQueue(&(sem->queue), currThread);
+		while (runQ == NULL) ;
+		swapcontext(&(currThread->context), &(runQ->context));
+	}
+}
+
+void V(struct semaphore *sem)
+{
+	sem->value++;
+	if (sem->value <= 0 && sem->queue != NULL) {
+		rotateQueue(&runQ);
+		addQueue(&runQ, delQueue(&(sem->queue)));
+	}
+	yield();
+}
+
+#endif
